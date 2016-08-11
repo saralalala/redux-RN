baffa8e369e1c42be572cd1b7b4ca19ca56c916b
sara <fenghuijun@chinamobile.com>
2016-08-11 13:06:30 +0800

    tt

--- Counter/app/containers/counterApp.js
+++ Counter/app/containers/counterApp.js
@@ -23,12 +23,14 @@ class CounterApp extends Component {
     );
   }
 }
-
-export default connect(state => ({
+//export default 
+CounterApp = connect(state => ({
     state: state.counter
     
   }),
   (dispatch) => ({
     actions: bindActionCreators(counterActions, dispatch) //把action创建函数绑定到dispatch()方法上
   })
-)(CounterApp);
+)(CounterApp);  //柯里化函数：首选connect是个可以执行两次的柯里化函数，第一次传入的参数相当于一系列的定制化东西，
+                 //第二次传入的是你要连接的React组件，然后返回一个新的React组件。
+module.exports=CounterApp;
